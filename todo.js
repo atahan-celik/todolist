6b3154a3fdd4a94febcb1fa03819b7eff7b0a8e4
Atahan Celik <117012766+atahan-celik@users.noreply.github.com>
2022-12-12 21:54:58 +0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,16 +1,16 @@
-// Variables
+// Değişkenler
 
 let todoItems = []
-const todoInput = document.querySelector(".todo-input")
-const completedTodosDiv = document.querySelector(".completed-todos")
-const uncompletedTodosDiv = document.querySelector(".uncompleted-todos")
+const todoInput = document.querySelector('.todo-input')
+const completedTodosDiv = document.querySelector('.completed-todos')
+const uncompletedTodosDiv = document.querySelector('.uncompleted-todos')
 const audio = new Audio('sound.mp3')
 
 
-// Get todo list on first boot
+// İlk açılışta yapılacaklar listesini alma
 window.onload = () => {
     let storageTodoItems = localStorage.getItem('todoItems')
-    if(storageTodoItems != null){
+    if(storageTodoItems != null) {
         todoItems = JSON.parse(storageTodoItems)
     }
 
@@ -18,9 +18,9 @@ window.onload = () => {
 
 }
 
-// Get the content typed into the input
+// veri girme
 todoInput.onkeyup = (e) => {
-    let value = e.target.value.replace(/^\s+/, "")
+    let value = e.target.value.replace(/^\s+/, "");
     if(value && e.keyCode === 13){ // Enter
         addTodo(value)
 
@@ -28,10 +28,10 @@ todoInput.onkeyup = (e) => {
         todoInput.focus()
     
     }
-}
+};
 
 
-// Add todo
+// Yapılacak iş ekleme
 function addTodo(text){
     todoItems.push({
         id: Date.now(),
@@ -42,49 +42,49 @@ function addTodo(text){
     saveAndRender()
 }
 
-// Remove todo
-function removeTodo(id){
-    todoItems = todoItems.filter(todo.id !== Number(id))
+// Yapılacak iş kaldırma
+function removeTodo(id) {
+    todoItems = todoItems.filter(todo => todo.id !== Number(id))
     saveAndRender()
 }
 
-// Mark as completed
-function markAsCompleted(id){
+// Tamamlandı olarak işaretleme
+function markAsCompleted(id) {
     todoItems = todoItems.filter(todo => {
-        if(todo.id === Number(id)){
+        if (todo.id === Number(id)) {
         todo.completed = true
         }
 
         return todo
     })
 
-    audio.play()
+    audio.play();
 
-    saveAndRender
+    saveAndRender()
 }
 
-// Mark as uncompleted
-function markAsUncompleted(id){
+// Tamamlanmadı olarak işaretleme
+function markAsUncompleted(id) {
     todoItems = todoItems.filter(todo => {
-        if(todo.id === Number(id)){
+        if (todo.id === Number(id)) {
         todo.completed = false
         }
 
         return todo
     })
 
-    saveAndRender
+    saveAndRender()
 }
 
-// Save in Localstorage
+// Yerel depolamaya kaydet
 function save(){
-    localStorage.setItem('todoItrems', JSON.stringify(todoItems))
+    localStorage.setItem('todoItems', JSON.stringify(todoItems))
 }
 
-// Render
-function render(){
+// İşleme
+function render() {
     let unCompletedTodos = todoItems.filter(item => !item.completed)
-    let completedTodos = todoItems.filter(item => !item.completed)
+    let completedTodos = todoItems.filter(item => item.completed)
 
     completedTodosDiv.innerHTML = ''
     uncompletedTodosDiv.innerHTML = ''
@@ -93,12 +93,12 @@ function render(){
         unCompletedTodos.forEach(todo => {
             uncompletedTodosDiv.append(createTodoElement(todo))
         })
-    }else{
-        uncompletedTodosDiv.innerHTML = '<div class="empty">Tamamlanmamış görev yok</div>'
+    } else {
+        uncompletedTodosDiv.innerHTML = `<div class='empty'>Tamamlanmamış görev yok</div>`
     }
 
     if(completedTodos.length > 0) {
-        completedTodosDiv.innerHTML = '<div class="completed-title">Tamamlanmış(${completedTodos} / ${todoItems.length})</div>'
+        completedTodosDiv.innerHTML = `<div class='completed-title'>Tamamlanmış (${completedTodos.length} / ${todoItems.length})</div>`
 
         completedTodos.forEach(todo => {
         completedTodosDiv.append(createTodoElement(todo))
@@ -108,49 +108,55 @@ function render(){
 
 }
 
-// Save and render
-function saveAndRender(){
-save()
-render()
+// Kaydetme ve İşleme
+function saveAndRender() {
+    save()
+    render()
+
 }
-// Create todo list item
-function createTodoElement(todo){
-    // Create todo list container
+// Yapılacaklar listesi öğesi oluşturma
+function createTodoElement(todo) {
+    // Tüm liste kapsayıcısını oluşturma
     const todoDiv = document.createElement('div')
     todoDiv.setAttribute('data-id', todo.id)
     todoDiv.className = 'todo-item'
 
-    // Create todo item text
+    // Tüm metin öğesini oluşturma
     const todoTexSpan = document.createElement('span')
     todoTexSpan.innerHTML = todo.text
 
-    // Checkbox for list
+
+    // Liste için onay kutusu
 
     const todoInputCheckbox = document.createElement('input')
-    todoInputCheckbox.type = 'checkbox'
-    todoInputCheckbox.checked = todo.completed
-    todoInputCheckbox.onclick = (e) => {
-        let id = e.target.closest('.todo-item').dataset.id
-        e.target.checked ? markAsCompleted(id) : markAsUncompleted(id)
+        todoInputCheckbox.type = 'checkbox'
+        todoInputCheckbox.checked = todo.completed
+        todoInputCheckbox.onclick = (e) => {
+            let id = e.target.closest('.todo-item').dataset.id
+            e.target.checked ? markAsCompleted(id) : markAsUncompleted(id)
     }
 
 
-  // Delete buttn for list
+  // Listeden silme butonu
+  const todoRemoveBtn = document.createElement('a')
+  todoRemoveBtn.href = '#'
+  todoRemoveBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                          <line x1="18" y1="6" x2="6" y2="18"></line>
+                          <line x1="6" y1="6" x2="18" y2="18"></line>
+                       </svg>`
 
-    const todoRemoveBtn = document.createElement('a')
-    todoRemoveBtn.href = '#'
-    todoRemoveBtn.innerHTML = ''
+  todoRemoveBtn.onclick = (e) => {
+  let id = e.target.closest('.todo-item').dataset.id
+  removeTodo(id)
+  }
+  
+  todoTexSpan.prepend(todoInputCheckbox)
+  todoDiv.appendChild(todoTexSpan)
+  todoDiv.appendChild(todoRemoveBtn)
 
-    todoRemoveBtn.onclick = (e) => {
-    let id = e.target.closest('.todo-item').dataset.id
-    removeTodo(id)
-    }
+  return todoDiv
     
-    todoTexSpan.prepend(todoInputCheckbox)
-    todoDiv.appendChild(todoTexSpan)
-    todoDiv.appendChild(todoRemoveBtn)
-
-    return todoDiv
 
 
 }
